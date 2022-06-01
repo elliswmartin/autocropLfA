545d672f45ac7cc2897af6e5799f91c8ef60396b
elliswmartin <54450015+elliswmartin@users.noreply.github.com>
2022-06-01 15:26:44 -0700

    Update makeJpg.sh

--- makeJpg.sh
+++ makeJpg.sh
@@ -1,15 +1,12 @@
 #!/bin/bash -x
 
-# make crop folder if it does not already exist
+# make crop folder if does not already exist
 cd ~/Desktop/ && mkdir -p "crop" 
-cd ~/Desktop/makeJpg/
+cd ~/Desktop/crop/
 
 echo 🪄creating jpgs, hold please 🚀
 
 # create flatten jpgs from tiffs
 mogrify -flatten -format jpg *.tif
 
-# move jpgs to destination folder
-mv *.jpg ~/Desktop/crop
-
-echo ⭐jpgs created and moved to crop folder. 
+echo ⭐jpgs created, processing complete. 
