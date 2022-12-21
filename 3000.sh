86fdb2fb39bec84940b3ce3e69250b09d547403e
elliswmartin <54450015+elliswmartin@users.noreply.github.com>
2022-12-21 14:00:16 -0800

    Delete 3000.sh

--- 3000.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-# add to folder where you want to resize to 3000 px on the longest edge.
-# then run script in terminal
-
-# resizes files to 3000 px on the longer side
-mogrify -resize 3000x3000\> *.jpg 
