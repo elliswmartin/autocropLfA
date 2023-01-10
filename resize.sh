a47a9727fa643e32d471e386f0679c92cea29f93
elliswmartin <54450015+elliswmartin@users.noreply.github.com>
2023-01-10 12:19:33 -0800

    Moved makeJpgs.sh and resize.sh to shhelpers repo

--- resize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-cd ~/Desktop/processed
-echo 🪚🪵 now on to downsizing, hold please 🛸
-
-# resize to 3000 pixels on the longest side, does not upscale. 
-mogrify -resize 3000x3000\> *.jpg 
-echo 🌱 all images resized. 
